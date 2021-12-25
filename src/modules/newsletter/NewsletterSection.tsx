9c16d622ea04d95471a6d90fd4b11b5e338b4736
carlogino <carlogino@ntucenterprise.sg>
2021-12-25 14:22:44 +0800

    feat(newsletter): updated message

--- src/modules/newsletter/NewsletterSection.tsx
+++ src/modules/newsletter/NewsletterSection.tsx
@@ -1,17 +1,56 @@
 import styled from '@emotion/styled';
+import Description from '../../components/typography/Paragraph';
+import Heading3 from '../../components/typography/Heading3';
+import {mq} from '../../utils/media-query';
 import Gap from '../../components/basic/Gap';
 
 const NewsletterSection = () => {
   return (
     <Container>
       <Gap color="dark" />
-      TEST
+      <Content>
+        <ContentMessage>
+          <Heading3>Keen for a heads up on upcoming events?</Heading3>
+          <Description>
+            Register your interest and we promise you’ll be the first to know
+            what we’ve got in store.
+          </Description>
+        </ContentMessage>
+        <FormWrapper>
+          <form>Form Here</form>
+        </FormWrapper>
+      </Content>
       <Gap color="dark" />
     </Container>
   );
 };
 
+const FormWrapper = styled.div({
+  position: 'relative',
+});
+
+const Content = styled.article(
+  mq({
+    display: 'flex',
+    justifyContent: ['flex-start', '', '', 'center'],
+    flexDirection: ['column', '', '', 'row'],
+    alignItems: ['center', '', '', 'flex-start'],
+    width: '100%',
+
+    gap: 32,
+  }),
+);
+
+const ContentMessage = styled.section({
+  padding: '0px 16px',
+  maxWidth: 480,
+  h3: {
+    marginTop: 0,
+  },
+});
+
 const Container = styled.div({
+  minHeight: 700,
   width: '100%',
   backgroundColor: 'var(--color-background-dark)',
   display: 'flex',
